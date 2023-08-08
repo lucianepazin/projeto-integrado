336d914b89785e90441fc498e500e127f8da19d0
lucianepazin <luciane.mauro@softplan.com.br>
2023-08-08 19:19:47 -0300

    commit intermediario pra atualizar api primeiro

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,15 @@
-import BasicCard from "@/components/shared/BasicCard";
-import { Container } from "@mui/material";
-import { Pet } from "domain/pet";
-
 export default async function Home() {
-  const {
-    data: { pets },
-  } = await fetch(process.env.API_URL + "api/dogs")
-    .then((res) => res.json())
-    .catch((e) => console.log(e));
-
-  return (
-    <Container sx={{ display: "flex", flexWrap: "wrap" }}>
-      {pets.map((pet: Pet) => (
-        <BasicCard pet={pet} key={pet.mouraId} />
-      ))}
-    </Container>
-  );
+  // const {
+  //   data: { pets },
+  // } = await fetch(process.env.API_URL + "api/dogs")
+  //   .then((res) => res.json())
+  //   .catch((e) => console.log(e));
+  return <div>oi</div>;
+  // return (
+  //   <Container sx={{ display: "flex", flexWrap: "wrap" }}>
+  //     {pets.map((pet: Pet) => (
+  //       <BasicCard pet={pet} key={pet.mouraId} />
+  //     ))}
+  //   </Container>
+  // );
 }
