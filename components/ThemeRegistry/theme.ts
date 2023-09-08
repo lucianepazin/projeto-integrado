162d7f9ae85dd2bd3c252f1a09dc1cff2109dfab
lucianepazin <luciane.mauro@softplan.com.br>
2023-09-07 22:31:24 -0300

    Adiciona estrutura, estilo e ícones

---
+++ components/ThemeRegistry/theme.ts
@@ -0,0 +1,33 @@
+import { createTheme } from "@mui/material/styles";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#C87FD3",
+    },
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+  components: {
+    MuiAlert: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.severity === "info" && {
+            backgroundColor: "#C87FD3",
+          }),
+        }),
+      },
+    },
+  },
+});
+
+export default theme;
